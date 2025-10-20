61423b4ccff0d32620245151372e5406118ef266
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 20:44:17 +0300

    Add extension method for registering application layer services

---
+++ ExpenseTracker.Application/DependencyInjection.cs
@@ -0,0 +1,43 @@
+﻿using ExpenseTracker.Application.Abstractions.CQRS.Handlers;
+using ExpenseTracker.Application.Categories.Commands.CreateCategoryCommands;
+using ExpenseTracker.Application.Categories.Commands.DeleteCategoryByIdCommands;
+using ExpenseTracker.Application.Categories.Queries.GetAllCategoriesQueries;
+using ExpenseTracker.Application.Categories.Queries.GetCategoryByIdQueries;
+using ExpenseTracker.Application.ExpenseRecords.Commands.CreateExpenseRecordCommands;
+using ExpenseTracker.Application.ExpenseRecords.Commands.DeleteExpenseRecordByIdCommands;
+using ExpenseTracker.Application.ExpenseRecords.Queries.GetExpenseRecordByIdQueries;
+using ExpenseTracker.Application.ExpenseRecords.Queries.GetExpenseRecordsByFilterQueries;
+using ExpenseTracker.Application.Users.Commands.CreateUserCommands;
+using ExpenseTracker.Application.Users.Commands.DeleteUserByIdCommands;
+using ExpenseTracker.Application.Users.Queries.GetAllUsersQueries;
+using ExpenseTracker.Application.Users.Queries.GetUserByIdQueries;
+using ExpenseTracker.Domain.Categories;
+using ExpenseTracker.Domain.ExpenseRecords;
+using ExpenseTracker.Domain.Users;
+using Microsoft.Extensions.DependencyInjection;
+using Shared.Results;
+
+namespace ExpenseTracker.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<ICommandHandler<CreateUserCommand, Result<Guid>>, CreateUserCommandHandler>();
+        services.AddScoped<ICommandHandler<DeleteUserByIdCommand, Result>, DeleteUserByIdCommandHandler>();
+        services.AddScoped<IQueryHandler<GetUserByIdQuery, Result<User>>, GetUserByIdQueryHandler>();
+        services.AddScoped<IQueryHandler<GetAllUsersQuery, IEnumerable<User>>, GetAllUsersQueryHandler>();
+        
+        services.AddScoped<ICommandHandler<CreateCategoryCommand, Result<Guid>>, CreateCategoryCommandHandler>();
+        services.AddScoped<ICommandHandler<DeleteCategoryByIdCommand, Result>, DeleteCategoryByIdCommandHandler>();
+        services.AddScoped<IQueryHandler<GetCategoryByIdQuery, Result<Category>>, GetCategoryByIdQueryHandler>();
+        services.AddScoped<IQueryHandler<GetAllCategoriesQuery, IEnumerable<Category>>, GetAllCategoriesQueryHandler>();
+
+        services.AddScoped<ICommandHandler<CreateExpenseRecordCommand, Result<Guid>>, CreateExpenseRecordCommandHandler>();
+        services.AddScoped<ICommandHandler<DeleteExpenseRecordByIdCommand, Result>, DeleteExpenseRecordByIdCommandHandler>();
+        services.AddScoped<IQueryHandler<GetExpenseRecordByIdQuery, Result<ExpenseRecord>>, GetExpenseRecordByIdQueryHandler>();
+        services.AddScoped<IQueryHandler<GetExpenseRecordsByFilterQuery, Result<IEnumerable<ExpenseRecord>>>, GetExpenseRecordsByFilterQueryHandler>();
+        
+        return services;
+    }
+}

8a9c73de6fb6eb55f8d778ef922b57a13c3b8eaa
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 19:47:04 +0300

    Add extension method for registering persistence layer services

---
+++ ExpenseTracker.Persistence/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using ExpenseTracker.Application.Categories.Abstractions;
+using ExpenseTracker.Application.ExpenseRecords.Abstractions;
+using ExpenseTracker.Application.Users.Abstractions;
+using ExpenseTracker.Persistence.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpenseTracker.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services)
+    {
+        services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+        services.AddSingleton<ICategoryRepository, InMemoryCategoryRepository>();
+        services.AddSingleton<IExpenseRecordRepository, InMemoryExpenseRecordRepository>();
+        
+        return services;
+    }
+}

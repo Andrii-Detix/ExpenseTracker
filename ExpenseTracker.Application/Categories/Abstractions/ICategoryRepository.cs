b7859fd98990cb1900df095cbad3b48c860210b9
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 13:48:25 +0300

    Add category use cases and category repository abstraction
    Add ICategoryRepository for data access;
    Add application use cases for:
    - creating a new category;
    - deleting a category by id;
    - getting a category by id;
    - getting all categories;

---
+++ ExpenseTracker.Application/Categories/Abstractions/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using ExpenseTracker.Domain.Categories;
+
+namespace ExpenseTracker.Application.Categories.Abstractions;
+
+public interface ICategoryRepository
+{
+    public Task<Category?> GetById(Guid id, CancellationToken ct);
+    public Task<IEnumerable<Category>> GetAll(CancellationToken ct);
+    public Task Add(Category category, CancellationToken ct);
+    public Task Delete(Guid id, CancellationToken ct);
+}
---
+++ ExpenseTracker.Application/Categories/Commands/CreateCategoryCommands/CreateCategoryCommand.cs
@@ -0,0 +1,6 @@
+﻿using ExpenseTracker.Application.Abstractions.CQRS.Requests;
+using Shared.Results;
+
+namespace ExpenseTracker.Application.Categories.Commands.CreateCategoryCommands;
+
+public record CreateCategoryCommand(string Name) : ICommand<Result<Guid>>;

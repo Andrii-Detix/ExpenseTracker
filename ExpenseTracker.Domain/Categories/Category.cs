15668c946c58f4414d1f88a6e8a51f90b856074b
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 11:56:13 +0300

    Add Category entity
    Include CategoryName value object and CategoryNameErrors for domain validation

---
+++ ExpenseTracker.Domain/Categories/Category.cs
@@ -0,0 +1,27 @@
+﻿using ExpenseTracker.Domain.Abstractions;
+using ExpenseTracker.Domain.Categories.ValueObjects.CategoryNames;
+using Shared.Results;
+
+namespace ExpenseTracker.Domain.Categories;
+
+public class Category : BaseEntity
+{
+    private Category(Guid id, CategoryName name)
+        : base(id)
+    {
+        Name = name;
+    }
+    
+    public CategoryName Name { get; }
+
+    public static Result<Category> Create(Guid id, string name)
+    {
+        Result<CategoryName> nameResult = CategoryName.Create(name);
+        if (nameResult.IsFailure)
+        {
+            return nameResult.Error;
+        }
+
+        return new Category(id, nameResult.Value!);
+    }
+}
---
+++ ExpenseTracker.Domain/Categories/ValueObjects/CategoryNames/CategoryName.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Results;
+
+namespace ExpenseTracker.Domain.Categories.ValueObjects.CategoryNames;
+
+public record CategoryName
+{
+    private CategoryName(string value)
+    {
+        Value = value;
+    }
+    
+    public string Value { get; }
+
+    public static Result<CategoryName> Create(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+        {
+            return CategoryNameErrors.IsEmpty;
+        }
+
+        return new CategoryName(name.Trim());
+    }
+}

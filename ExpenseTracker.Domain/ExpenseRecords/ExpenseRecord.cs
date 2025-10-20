0f67dfb2aab9c3464e7a011867f01af4329c4f18
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 11:57:05 +0300

    Add ExpenseRecord entity
    Include ExpenseAmount value object and ExpenseAmountErrors for domain validation

---
+++ ExpenseTracker.Domain/ExpenseRecords/ExpenseRecord.cs
@@ -0,0 +1,43 @@
+﻿using ExpenseTracker.Domain.Abstractions;
+using ExpenseTracker.Domain.ExpenseRecords.ValueObjects.ExpenseAmounts;
+using Shared.Results;
+
+namespace ExpenseTracker.Domain.ExpenseRecords;
+
+public class ExpenseRecord : BaseEntity
+{
+    private ExpenseRecord(
+        Guid id,
+        Guid userId,
+        Guid categoryId,
+        DateTime createdAt,
+        ExpenseAmount amount)
+        : base(id)
+    {
+        UserId = userId;
+        CategoryId = categoryId;
+        CreatedAt = createdAt;
+        Amount = amount;
+    }
+    
+    public Guid UserId { get; }
+    public Guid CategoryId { get; }
+    public DateTime CreatedAt { get; }
+    public ExpenseAmount Amount { get; }
+
+    public static Result<ExpenseRecord> Create(
+        Guid id,
+        Guid userId,
+        Guid categoryId,
+        DateTime createdAt,
+        decimal amount)
+    {
+        Result<ExpenseAmount> amountResult = ExpenseAmount.Create(amount);
+        if (amountResult.IsFailure)
+        {
+            return amountResult.Error;
+        }
+
+        return new ExpenseRecord(id, userId, categoryId, createdAt, amountResult.Value!);
+    }
+}
---
+++ ExpenseTracker.Domain/ExpenseRecords/ValueObjects/ExpenseAmounts/ExpenseAmount.cs
@@ -0,0 +1,27 @@
+﻿using Shared.Results;
+
+namespace ExpenseTracker.Domain.ExpenseRecords.ValueObjects.ExpenseAmounts;
+
+public record ExpenseAmount
+{
+    private const int DecimalPlaces = 2;
+    
+    private ExpenseAmount(decimal value)
+    {
+        Value = value;
+    }
+    
+    public decimal Value { get; }
+
+    public static Result<ExpenseAmount> Create(decimal value)
+    {
+        if (value < 0)
+        {
+            return ExpenseAmountErrors.IsNegative;
+        }
+
+        value = Math.Round(value, DecimalPlaces, MidpointRounding.AwayFromZero);
+        
+        return new ExpenseAmount(value);
+    }
+}

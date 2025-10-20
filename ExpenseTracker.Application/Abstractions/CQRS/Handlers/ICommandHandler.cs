23ddeefa82c4f8afd09fc061dfacbf90d7ab6010
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 12:29:30 +0300

    Add CQRS abstractions
    Add ICommand, IQuery for creation request objects;
    Add ICommandHandler, IQueryHandler for handling requests;

---
+++ ExpenseTracker.Application/Abstractions/CQRS/Handlers/ICommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using ExpenseTracker.Application.Abstractions.CQRS.Requests;
+
+namespace ExpenseTracker.Application.Abstractions.CQRS.Handlers;
+
+public interface ICommandHandler<in TCommand>
+    where TCommand : ICommand
+{
+    Task Handle(TCommand command, CancellationToken ct);
+}
+
+public interface ICommandHandler<in TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+{
+    Task<TResponse> Handle(TCommand command, CancellationToken ct);
+}
---
+++ ExpenseTracker.Application/Abstractions/CQRS/Requests/IQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpenseTracker.Application.Abstractions.CQRS.Requests;
+
+public interface IBaseQuery;
+
+public interface IQuery : IBaseQuery;
+
+public interface IQuery<TResponse> : IBaseQuery;

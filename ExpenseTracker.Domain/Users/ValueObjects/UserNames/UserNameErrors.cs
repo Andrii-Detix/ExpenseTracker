2b87bb9f54b6bb988392f2d654a24f21bdb88b0d
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 11:54:47 +0300

    Add User entity
    Add domain User entity with UserName value object and UserNameErrors

---
+++ ExpenseTracker.Domain/Users/ValueObjects/UserNames/UserNameErrors.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Results.Errors;
+
+namespace ExpenseTracker.Domain.Users.ValueObjects.UserNames;
+
+public static class UserNameErrors
+{
+    public static readonly Error IsEmpty = Error.Validation(
+        "UserName.Empty", "The user name cannot be empty.");
+    
+    public static Error LengthOutOfRange(int min, int max) => Error.Validation(
+        "UserName.LengthOutOfRange", $"The length of the user name must be between {min} and {max}.");
+
+    public static readonly Error MustStartWithCapital = Error.Validation(
+        "UserName.MustStartWithCapital", "The user name must start with capital letter.");
+    
+    public static readonly Error InvalidFormat = Error.Validation(
+        "UserName.InvalidFormat", "The user name must contain only letters and symbols \"-\", \"'\"");
+}

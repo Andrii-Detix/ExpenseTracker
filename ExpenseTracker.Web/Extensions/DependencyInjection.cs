3cf1ee6e28c8cc9f51cdf1fa8a875e5f22a50710
Andrii-Detix <aivancisin6@gmail.com>
2025-10-20 21:36:00 +0300

    Add extension method for registering web layer services

---
+++ ExpenseTracker.Web/Extensions/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace ExpenseTracker.Web.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWeb(this IServiceCollection services)
+    {
+        services.AddProblemDetails(options =>
+        {
+            options.CustomizeProblemDetails = context =>
+            {
+                context.ProblemDetails.Instance =
+                    $"{context.HttpContext.Request.Method} {context.HttpContext.Request.Path}";
+            };
+        });
+        
+        return services;
+    }
+}

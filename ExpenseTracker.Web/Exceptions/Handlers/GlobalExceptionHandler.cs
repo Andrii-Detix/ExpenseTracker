c02f1397d6f5a00c8e4359527081d3927601a47d
Andrii-Detix <aivancisin6@gmail.com>
2025-10-25 18:18:34 +0300

    Add global exception handler to manage unexpected runtime errors
    - Define global error instance;
    - Implement global exception handler;

---
+++ ExpenseTracker.Web/Exceptions/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using ExpenseTracker.Web.Exceptions.Errors;
+using ExpenseTracker.Web.Extensions;
+using Microsoft.AspNetCore.Diagnostics;
+using Shared.Results;
+
+namespace ExpenseTracker.Web.Exceptions.Handlers;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var problem = Result
+            .Failure(GlobalErrors.Occured)
+            .ToProblemDetails();
+
+        await problem.ExecuteAsync(httpContext);
+        
+        return true;
+    }
+}

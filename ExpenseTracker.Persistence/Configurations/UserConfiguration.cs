a7169ba5dce28d6b2cf1def6169ddc678cd4f6a9
Andrii-Detix <aivancisin6@gmail.com>
2025-10-25 11:14:53 +0300

    Add EF Core configurations for Category, ExpenseRecord, and User entities

---
+++ ExpenseTracker.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using ExpenseTracker.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExpenseTracker.Persistence.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+        
+        builder.HasKey(u => u.Id)
+            .HasName("pk_users");
+
+        builder.Property(u => u.Id)
+            .IsRequired()
+            .ValueGeneratedNever()
+            .HasColumnName("id");
+
+        builder.OwnsOne(u => u.Name, nameBuilder =>
+        {
+            nameBuilder.Property(n => n.Value)
+                .IsRequired()
+                .HasMaxLength(400)
+                .HasColumnName("name");
+        });
+    }
+}

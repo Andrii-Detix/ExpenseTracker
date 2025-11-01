f8300c121b3c1440e6ea8cea2ca55fcb475aa74f
Andrii-Detix <aivancisin6@gmail.com>
2025-11-01 20:13:57 +0200

    Add implementation of JwtProvider to generate JWT tokens

---
+++ ExpenseTracker.Infrastructure/Authentication/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace ExpenseTracker.Infrastructure.Authentication;
+
+public record JwtOptions
+{
+    public required string Issuer { get; init; }
+    public required string Audience { get; init; }
+    public required TimeSpan TokenLifetime { get; init; }
+    public required string SecretKey { get; init; }
+}
---
+++ ExpenseTracker.Infrastructure/Authentication/JwtProvider.cs
@@ -0,0 +1,40 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ExpenseTracker.Application.Abstractions.Jwt;
+using ExpenseTracker.Domain.Users;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ExpenseTracker.Infrastructure.Authentication;
+
+public sealed class JwtProvider(IOptions<JwtOptions> jwtOptions) 
+    : IJwtProvider
+{
+    private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+    
+    public string GenerateToken(User user)
+    {
+        Claim[] claims = 
+            [
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.UniqueName, user.Login.Value)
+            ];
+
+        SigningCredentials signingCredentials = new(
+            new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_jwtOptions.SecretKey)),
+            SecurityAlgorithms.HmacSha256);
+
+        JwtSecurityToken token = new(
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
+            claims: claims,
+            expires: DateTime.UtcNow.Add(_jwtOptions.TokenLifetime),
+            signingCredentials: signingCredentials);
+        
+        string tokenValue = new JwtSecurityTokenHandler().WriteToken(token);
+        
+        return tokenValue;
+    }
+}

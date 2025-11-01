e22e31ea1a241239a5f27639d4e776462d5ca025
Andrii-Detix <aivancisin6@gmail.com>
2025-11-01 11:09:21 +0200

    Add IPasswordHasher interface and implementation for secure password hashing

---
+++ ExpenseTracker.Application/Abstractions/Security/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpenseTracker.Application.Abstractions.Security;
+
+public interface IPasswordHasher
+{
+    public string Generate(string password);
+    public bool Verify(string password, string hash);
+}
---
+++ ExpenseTracker.Infrastructure/Security/PasswordHashers/PasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using ExpenseTracker.Application.Abstractions.Security;
+
+namespace ExpenseTracker.Infrastructure.Security.PasswordHashers;
+
+public class PasswordHasher : IPasswordHasher
+{
+    public string Generate(string password)
+    {
+        return BCrypt.Net.BCrypt.EnhancedHashPassword(password);
+    }
+
+    public bool Verify(string password, string hash)
+    {
+        return BCrypt.Net.BCrypt.EnhancedVerify(password, hash);
+    }
+}

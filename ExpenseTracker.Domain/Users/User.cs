f7b4d1034b28bc35272c9384e2f0d082fdee0807
Andrii-Detix <aivancisin6@gmail.com>
2025-11-01 09:50:53 +0200

    Add value objects for user authentication
    Add Login, Password, PasswordHash

--- ExpenseTracker.Domain/Users/User.cs
+++ ExpenseTracker.Domain/Users/User.cs
@@ -1,4 +1,6 @@
 ﻿using ExpenseTracker.Domain.Abstractions;
+using ExpenseTracker.Domain.Users.ValueObjects.PasswordHashes;
+using ExpenseTracker.Domain.Users.ValueObjects.UserLogins;
 using ExpenseTracker.Domain.Users.ValueObjects.UserNames;
 using Shared.Results;
 
@@ -6,27 +8,43 @@ namespace ExpenseTracker.Domain.Users;
 
 public class User : BaseEntity
 {
-    private User(Guid id, Guid defaultCurrencyId, UserName name) 
+    private User(Guid id, UserLogin login, PasswordHash passwordHash, UserName name, Guid defaultCurrencyId) 
         : base(id)
     {
-        DefaultCurrencyId = defaultCurrencyId;
+        Login = login;
+        PasswordHash = passwordHash;
         Name = name;
+        DefaultCurrencyId = defaultCurrencyId;
     }
     
     private User() { }
     
-    public Guid DefaultCurrencyId { get; private set; }
+    public UserLogin Login { get; }
+    public PasswordHash PasswordHash { get; }
     public UserName Name { get; }
+    public Guid DefaultCurrencyId { get; private set; }
 
-    public static Result<User> Create(Guid id, Guid defaultCurrencyId, string name)
+    public static Result<User> Create(Guid id, string login, string passwordHash, string name, Guid defaultCurrencyId)
     {
+        Result<UserLogin> loginResult = UserLogin.Create(login);
+        if (loginResult.IsFailure)
+        {
+            return loginResult.Error;
+        }
+        
+        Result<PasswordHash> passwordHashResult = PasswordHash.Create(passwordHash);
+        if (passwordHashResult.IsFailure)
+        {
+            return passwordHashResult.Error;
+        }
+        
         Result<UserName> nameResult = UserName.Create(name);
         if (nameResult.IsFailure)
         {
             return nameResult.Error;
         }
 
-        return new User(id, defaultCurrencyId, nameResult.Value!);
+        return new User(id, loginResult.Value!, passwordHashResult.Value!, nameResult.Value!, defaultCurrencyId);
     }
 
     public void SetDefaultCurrency(Guid defaultCurrencyId)

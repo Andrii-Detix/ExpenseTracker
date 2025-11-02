77062d8bdd64cc3124b8d02fb805674841a73cf3
Andrii-Detix <aivancisin6@gmail.com>
2025-11-02 17:30:06 +0200

    Add ef core migrations for new authentication fields in User

---
+++ ExpenseTracker.Persistence/Migrations/20251102111813_Update user with properties for authentication.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpenseTracker.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updateuserwithpropertiesforauthentication : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "login",
+                table: "users",
+                type: "character varying(400)",
+                maxLength: 400,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "password_hash",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "ux_users_login",
+                table: "users",
+                column: "login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "ux_users_login",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "login",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "password_hash",
+                table: "users");
+        }
+    }
+}
